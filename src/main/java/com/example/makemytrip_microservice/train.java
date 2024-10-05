3fc1d94d9f0594cb201c627c88e6dd97399f16e3
Satyam Pandey <satyampandey88@gmail.com>
2024-10-05 12:37:16 +0530

    updating new train java class

--- src/main/java/com/example/makemytrip_microservice/train.java
+++ src/main/java/com/example/makemytrip_microservice/train.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class hotel {
-    @GetMapping("/hotel")
-    public String getData() {return  "Please book your hotels  in Pune & Mumbai at 10% discount" ; }
+public class train {
+    @GetMapping("/train")
+    public String getData() {return  "Please book your hotels  in Pune at 10% discount" ; }
 }
