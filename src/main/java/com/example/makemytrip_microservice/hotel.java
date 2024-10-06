5a72210e7250e4054e60779312ef171499186b8e
Satyam Pandey <satyampandey88@gmail.com>
2024-10-06 08:54:13 +0530

    hello

--- src/main/java/com/example/makemytrip_microservice/hotel.java
+++ src/main/java/com/example/makemytrip_microservice/hotel.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class hotel {
     @GetMapping("/hotel")
-    public String getData() {return  "Please book your hotels  in Pune & Mumbai & kolkatta at 20% discount" ; }
+    public String getData() {return  "Please book your hotels  in Pune & Mumbai & kolkatta dellhi at 20% discount" ; }
 }
